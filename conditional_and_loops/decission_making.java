5ca0e9b6db9d9cc5567bd941439c38326a7c2c9a
RafliRadianto <80236988+RafliRadianto@users.noreply.github.com>
2021-04-06 15:39:55 +0700

    Delete conditional_and_loops directory

--- conditional_and_loops/decission_making.java
+++
@@ -1,9 +0,0 @@
-package conditional_and_loops;
-
-public class decission_making {
-    public static void main(String[] args) {
-        int x = 5;
-        if (x == 5);
-        System.out.println(x);
-    }
-}
